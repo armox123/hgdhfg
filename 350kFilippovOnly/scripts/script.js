83344eba9d62b3a6799bc0c6f054d82a3113f72f
armox123 <104302875+armox123@users.noreply.github.com>
2026-06-02 23:22:28 +0700

    Add files via upload

--- 350kFilippovOnly/scripts/script.js
+++ 350kFilippovOnly/scripts/script.js
@@ -3,8 +3,8 @@
 let products = [
     {
         "id":1,
-        "name": "potato",
-        "discription": "Описание товара potato",
+        "name": "Картофель",
+        "discription": "Описание товара картофеля",
         "fullDiscription": "Карто́фель, или паслён клубнено́сный (лат. Solánum tuberósum), — вид многолетних клубненосных травянистых растений из рода Паслён (Solanum) семейства Паслёновые (Solanaceae). Клубни картофеля являются важным пищевым продуктом. Плоды ядовиты в связи с содержанием в них соланина. С потребительской точки зрения картофель является овощем.",
         "image": "./images/potato.webp",
         "filtr": "vegetables",
@@ -12,17 +12,17 @@ let products = [
     },
     {
         "id":2,
-        "name": "carrot",
-        "discription": "Описание товара carrot",
-        "full-discription": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis omnis dolores libero atque placeat adipisci nesciunt necessitatibus, repellat dignissimos suscipit? Necessitatibus quisquam voluptatibus ad ducimus vitae quae, praesentium hic perspiciatis?",
+        "name": "Морковь",
+        "discription": "Описание товара моркови",
+        "fullDiscription": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis omnis dolores libero atque placeat adipisci nesciunt necessitatibus, repellat dignissimos suscipit? Necessitatibus quisquam voluptatibus ad ducimus vitae quae, praesentium hic perspiciatis?",
         "image": "./images/carrot.jpg",
         "filtr": "vegetables",
         "price": 250,
     },
     {
         "id":3,
-        "name": "apple",
-        "discription": "Описание товара apple",
+        "name": "Яблоко",
+        "discription": "Описание товара яблоко",
         "fullDiscription": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis omnis dolores libero atque placeat adipisci nesciunt necessitatibus, repellat dignissimos suscipit? Necessitatibus quisquam voluptatibus ad ducimus vitae quae, praesentium hic perspiciatis?",
         "image": "./images/apple.jpg",
         "filtr": "fruit",
@@ -30,47 +30,71 @@ let products = [
     },
     {
         "id":4,
-        "name": "chicken",
-        "discription": "Описание товара chicken",
+        "name": "Курица",
+        "discription": "Описание товара курица",
         "fullDiscription": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis omnis dolores libero atque placeat adipisci nesciunt necessitatibus, repellat dignissimos suscipit? Necessitatibus quisquam voluptatibus ad ducimus vitae quae, praesentium hic perspiciatis?",
+        "image": "./images/chicken.jpg",
         "filtr": "meat",
         "price": 350,
     },
     {
         "id":5,
-        "name": "beef",
-        "discription": "Описание товара beef",
+        "name": "Говядина",
+        "discription": "Описание товара говядина",
         "fullDiscription": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis omnis dolores libero atque placeat adipisci nesciunt necessitatibus, repellat dignissimos suscipit? Necessitatibus quisquam voluptatibus ad ducimus vitae quae, praesentium hic perspiciatis?",
+        "image": "./images/beef.jpeg",
         "filtr": "meat",
         "price": 450,
     },
     {
         "id":6,
-        "name": "codfish",
-        "discription": "Описание товара codfish",
+        "name": "Треска",
+        "discription": "Описание товара треска",
         "fullDiscription": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis omnis dolores libero atque placeat adipisci nesciunt necessitatibus, repellat dignissimos suscipit? Necessitatibus quisquam voluptatibus ad ducimus vitae quae, praesentium hic perspiciatis?",
+        "image": "./images/cod.jpg",
         "filtr": "fish",
         "price": 600,
     },
 ] 
+let lastFlt;
+let query = "";
 const content = document.querySelector("#content");
-for (const product of products) {
-    content.innerHTML += 
-    `<div class="card">
-        <div class="img-div"><img class="cardImg" src='${product.image}', width=150px/></div>
-        <div><a href="product.html?id=${product.id}">${product.name}</a></div>
-        <div>${product.discription}</div>
-        <div>${product.price}</div>
-        <button>Добавить в корзину</button>
-    </div>`;
+function generate(flt, srch){
+    content.innerHTML = '';
+    lastFlt = flt;
+    for (const product of products) {
+        if(flt === 'all' && product.name.toLowerCase().includes(srch)){
+            content.innerHTML += 
+            `<div class="card">
+                <div class="img-div"><img class="cardImg" src='${product.image}', width=150px/></div>
+                <div><a href="product.html?id=${product.id}">${product.name}</a></div>
+                <div>${product.discription}</div>
+                <div>${product.price}</div>
+                <button>Добавить в корзину</button>
+            </div>`;
+        }
+        else if(flt === product.filtr && product.name.toLowerCase().includes(srch)){
+            content.innerHTML += 
+            `<div class="card">
+                <div class="img-div"><img class="cardImg" src='${product.image}', width=150px/></div>
+                <div><a href="product.html?id=${product.id}">${product.name}</a></div>
+                <div>${product.discription}</div>
+                <div>${product.price}</div>
+                <button>Добавить в корзину</button>
+            </div>`;
+        }
+        
+    }
 }
+const searchText = document.querySelector('#card-search')
+searchText.addEventListener('input', function(){
+    query = searchText.value.toLowerCase();
+    generate(lastFlt, query)
+})
 const filtr = document.querySelectorAll("input")
 for (const elem of filtr) {
     elem.addEventListener('change', function(changed){
-        if(changed.target.value === "all"){
-            
-        }
+        generate(changed.target.id, query);
     })
 }
-
-
+generate('all', "");
